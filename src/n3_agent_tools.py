91798c538b5c275bd269cedfd60648f2c8f9d977
Aleix Benet <benetaltona@gmail.com>
2026-06-04 11:26:55 +0200

    Change return value to model_dump_json method

--- src/n3_agent_tools.py
+++ src/n3_agent_tools.py
@@ -181,7 +181,7 @@ def rag_agent(query: str, max_turns: int = 5) -> str:
         temperature=0
     )
 
-    return final_parse.choices[0].message.parsed.answer
+    return final_parse.choices[0].message.parsed.model_dump_json()
 
 
 
